26640c3c4036476daf1b5a6cc4bf4279fdaed6df
Rahul Dolai <rahuldolai004@gmail.com>
2026-08-23 08:17:15 +0530

    Time: 2 ms (91.96%), Space: 48 MB (20.55%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,23 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int slow = -1;
-        for( int i = 0 ; i< nums.length ; i++ ){
-           // initialize slow
-           if(nums[i] == 0 && slow < 0){
-            slow = i ;
-           }
-           //slow is not initialize yet
-           if(slow == -1 ){
-            continue;
-           }
-           // case 1 slow=0, i non zero
-           if( nums[slow] ==0 && nums[i] != 0 ){
-            nums[slow] = nums[i] ;
-            nums[i] = 0;
-            slow++;
-           }
-          
-
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != 0) {
+                int temp = nums[slow];
+                nums[slow] = nums[fast];
+                nums[fast] = temp;
+                slow++;
+            }
         }
     }
 }
